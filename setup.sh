61899ea82eb879cd59b619587c5883953b7b55dc
Bart Willemsen <b.willemsen8@gmail.com>
2014-04-04 12:58:19 +0200

    More work on config scripts
    More package installation, changed MySQL configuration and set Ubuntu
    ServerName

--- setup.sh
+++ setup.sh
@@ -5,15 +5,25 @@ sudo apt-get update
 sudo apt-get upgrade -y
 
 # Install web packages.
-sudo apt-get install -y build-essential dkms apache2 php5 php5-xdebug php5-sqlite php5-mysql openssh-server \
-git vim subversion mysql-server
+sudo apt-get install -y build-essential dkms apache2 php5 php-pear php5-xdebug php5-sqlite php5-mysql \
+curl php5-curl memcached php5-memcached openssh-server git vim subversion
 
 # Download Bash Aliases
-wget -O ~/.bash_aliases https://gist.githubusercontent.com/Bartwillemsen/e6c672cdea193710e73b/raw/b52ccebf1982083d10ff27429f7174419dd19238/bash-aliases
+wget -O ~/.bash_aliases https://raw.githubusercontent.com/Bartwillemsen/virtual-devbox/master/aliases
+
+# Set Apache ServerName
+sudo sed -i "s/#ServerRoot.*/ServerName ubuntu/" /etc/apache2/apache2.conf
+sudo service apache restart
+
+# Install MySQL
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password secret'
+sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password secret'
+sudo apt-get -y install mysql-server
+
 
 # Configure MySQL.
-sudo sed  -i '/^bind-address/s/bind-address.*=.*/bind-address = 0.0.0.0' /etc/mysql/my.cnf
-mysql -u root -e "GRANT ALL ON *.* TO 'root'@'%'"
+sudo sed -i '/^bind-address/s/bind-address.*=.*/bind-address = 10.0.2.15/' /etc/mysql/my.cnf
+mysql -u root -p mysql -e "GRANT ALL ON *.* TO root@'10.0.2.2' IDENTIFIED BY 'secret';"
 sudo service mysql restart
 
 # Create Scripts directory.
@@ -21,7 +31,7 @@ mkdir ~/Scripts
 mkdir ~/Scripts/PhpInfo
 
 # Download Serve script.
-wget -O ~/Scripts/serve.sh https://gist.githubusercontent.com/Bartwillemsen/9630220/raw/f27a094072b9affc321dfff856457fc81cdfe3b9/serve-script.sh
+wget -O ~/Scripts/serve.sh https://raw.githubusercontent.com/Bartwillemsen/virtual-devbox/master/serve.sh
 
 # Create PHP Info site.
 echo "<?php echo phpinfo();" > ~/Scripts/PhpInfo/index.php
@@ -29,7 +39,6 @@ echo "<?php echo phpinfo();" > ~/Scripts/PhpInfo/index.php
 #Configure VirtualHosts
 sudo a2enmod rewrite
 echo "127.0.0.1  info.app" | sudo tee -a /etc/hosts
-
 vhost="<VirtualHost *:80>
     ServerName info.app
 	DocumentRoot /home/bart/Scripts/PhpInfo
@@ -40,7 +49,6 @@ vhost="<VirtualHost *:80>
 		AllowOverride all
 	</Directory>
 </VirtualHost>"
-
 echo "$vhost" | sudo tee /etc/apache2/sites-available/info.app.conf
 sudo a2ensite info.app
 sudo service apache2 restart
@@ -48,11 +56,8 @@ sudo service apache2 restart
 #VirtualBox Guest Additions.
 sudo mount /dev/cdrom /media/cdrom
 sudo bash /media/cdrom/VBoxLinuxAdditions.run
-
-# Add www-data and own useraccount to vboxsf group to get access to
-# shared folder.
-sudo usermod -a -G vboxsf www-data
-sudo usermod -a -G vboxsf bart
+sudo usermod -aG vboxsf www-data
+sudo usermod -aG vboxsf bart
 
 # Finally, reboot to apply all changes.
 sudo reboot
