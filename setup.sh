a36e152b292f987e2dfdcc64f17a110ba3f0907b
Bart Willemsen <b.willemsen8@gmail.com>
2014-05-10 15:14:14 +0200

    Accidentally deleted ini extension. Oops!

--- setup.sh
+++ setup.sh
@@ -31,7 +31,7 @@ sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
 sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/cli/php.ini
 sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/cli/php.ini
-sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/cli/php.
+sudo sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php5/cli/php.ini
 
 # Generate SSH Key
 cd ~
